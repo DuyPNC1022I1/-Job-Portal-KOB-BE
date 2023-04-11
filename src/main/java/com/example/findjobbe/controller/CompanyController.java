00f682605fc987e79fa7617ae58f9878b27aee33
Duc Manh <manhduc.nong93@gmail.com>
2023-04-11 11:22:12 +0700

    update company lan 1

---
+++ src/main/java/com/example/findjobbe/controller/CompanyController.java
@@ -0,0 +1,51 @@
+package com.example.findjobbe.controller;
+
+import com.example.findjobbe.model.Company;
+import com.example.findjobbe.model.Job;
+import com.example.findjobbe.service.company.CompanyService;
+import com.example.findjobbe.service.jobs.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/companies")
+public class CompanyController {
+    @Autowired
+    private CompanyService companyService;
+
+    @GetMapping
+    public ResponseEntity<Page<Company>> findAll(@PageableDefault(page = 5) Pageable pageable, @RequestParam("search") Optional<String> name) {
+        Page<Company> companies;
+        if (name.isPresent()) {
+            companies = companyService.findAll(name.get(), pageable);
+        } else {
+            companies = companyService.findAll("", pageable);
+        }
+        return new ResponseEntity<> (companies, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Company> findOne(@PathVariable Long id) {
+        return new ResponseEntity<>(companyService.findOne(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody Company company) {
+        companyService.save(company);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        companyService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
--- src/main/java/com/example/findjobbe/service/ICoreService.java
+++ src/main/java/com/example/findjobbe/service/ICoreService.java
@@ -4,7 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface ICoreService<E> {
-    Page<E> findAll(String name, Pageable pageable);
+    Page<E> findAll(Pageable pageable);
     E findOne(Long id);
     void save(E e);
     void delete(Long id);
