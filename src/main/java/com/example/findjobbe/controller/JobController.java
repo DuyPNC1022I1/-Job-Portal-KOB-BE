ee956361af90f26f61ede28fc866ce1cd185ef3e
DuyPNC1022I1 <duypn1408@gmail.com>
2023-04-12 03:23:31 +0700

    Update block or unlock job

--- src/main/java/com/example/findjobbe/controller/JobController.java
+++ src/main/java/com/example/findjobbe/controller/JobController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -39,7 +40,12 @@ public class JobController {
 
     @PostMapping
     public ResponseEntity<Void> save(@RequestBody Job job) {
-        jobService.save(job);
+        job.setStartDate(java.time.LocalDate.now());
+        try {
+            jobService.save(job);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
@@ -49,9 +55,31 @@ public class JobController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    //Get data listJob by Company
     @GetMapping("/findByCompany/{id}")
     public ResponseEntity<List<Job>> findAllByCompany(@PathVariable Long id) {
         return new ResponseEntity<>(jobService.findAllByCompany(id), HttpStatus.OK);
     }
 
+    //Block Job
+    @GetMapping("/blockOrUnlockJob/{id}")
+    public ResponseEntity<List<Job>> blockOrUnlockJob(@PathVariable Long id) {
+        Job jobBlock = jobService.findOne(id);
+        if (jobBlock.getStatus()) {
+            jobBlock.setStatus(false); //block job
+        } else {
+            jobBlock.setStatus(true); //unlock job
+        }
+        jobService.save(jobBlock);
+        return new ResponseEntity<>(jobService.findAllTest(), HttpStatus.OK);
+    }
+
+    //Unlock job
+    @GetMapping("/unlockJob/{id}")
+    public ResponseEntity<List<Job>> unlockJob(@PathVariable Long id) {
+        Job jobUnlock = jobService.findOne(id);
+        jobUnlock.setStatus(true);
+        jobService.save(jobUnlock);
+        return new ResponseEntity<>(jobService.findAllTest(), HttpStatus.OK);
+    }
 }
