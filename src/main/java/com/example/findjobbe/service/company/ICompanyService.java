8b610d32b7cd9864c51def0da9985cb49939e764
Duc Manh <manhduc.nong93@gmail.com>
2023-04-11 15:02:39 +0700

    update lai company lan 2

--- src/main/java/com/example/findjobbe/service/company/ICompanyService.java
+++
@@ -1,7 +0,0 @@
-package com.example.findjobbe.service.company;
-
-import com.example.findjobbe.model.Company;
-import com.example.findjobbe.service.ICoreService;
-
-public interface ICompanyService extends ICoreService<Company> {
-}
