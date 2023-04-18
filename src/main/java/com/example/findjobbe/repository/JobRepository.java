7b6794952f864602af827f443819a491ef0d0d75
DuyPNC1022I1 <duypn1408@gmail.com>
2023-04-18 09:18:52 +0700

    Update

--- src/main/java/com/example/findjobbe/repository/JobRepository.java
+++ src/main/java/com/example/findjobbe/repository/JobRepository.java
@@ -3,6 +3,8 @@ package com.example.findjobbe.repository;
 import com.example.findjobbe.model.Career;
 import com.example.findjobbe.model.City;
 import com.example.findjobbe.model.Job;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,6 +16,16 @@ import java.util.List;
 @Repository
 public interface JobRepository extends JpaRepository<Job,Long> {
     List<Job> findAllByCompany_Id(Long companyId);
+    Page<Job> findAllByCareer_Id(Long id, Pageable pageable);
+    @Query("SELECT j FROM Job j WHERE j.company.account.name like : name")
+    Page<Job> findAllJobByName(@Param("name") String name, Pageable pageable);
+    @Query("SELECT j FROM Job j WHERE j.company.address like : address")
+    Page<Job> findAllJobByCompanyAddress(@Param("address") String address,Pageable pageable);
+    Page<Job> findAllByCompany_City_Id(Long id,Pageable pageable);
+    Page<Job> findAllByEmployeeType_Id(Long id,Pageable pageable);
+
+
+
     List<Job> findAllByCareer_Id(Long id);
     @Query("SELECT j FROM Job j WHERE j.company.account.name like : name")
     List<Job> findAllJobByName(@Param("name") String name);
