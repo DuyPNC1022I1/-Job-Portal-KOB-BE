3c512d8e8240f12cef22311106091b5462b88baf
DuyPNC1022I1 <duypn1408@gmail.com>
2023-04-19 08:59:47 +0700

    Update searchkey

---
+++ src/main/java/com/example/findjobbe/model/SearchKey.java
@@ -0,0 +1,15 @@
+package com.example.findjobbe.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchKey {
+    private String key1;
+    private String key2;
+}
--- src/main/java/com/example/findjobbe/service/jobs/JobService.java
+++ src/main/java/com/example/findjobbe/service/jobs/JobService.java
@@ -12,131 +12,139 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class JobService extends ICoreServiceJob{
+public class JobService extends ICoreServiceJob {
     @Autowired
     private JobRepository jobRepository;
+
     public List<Job> findAllTest() {
         return jobRepository.findAll();
     }
 
-    public Page<Job> findALl(Pageable pageable){
+    public Page<Job> findALl(Pageable pageable) {
         return jobRepository.findAll(pageable);
     }
+
     public List<Job> findAllByCompany(Long companyId) {
         return jobRepository.findAllByCompany_Id(companyId);
     }
-    public Page<Job> findAllByJobName(String name, Pageable pageable){
-        return jobRepository.findAllJobByName(name,pageable);
+
+    public Page<Job> findAllByJobName(String name, Pageable pageable) {
+        return jobRepository.findAllJobByName(name, pageable);
     }
-    public Page<Job> findALlByCompanyAddress(String address,Pageable pageable){
-        return jobRepository.findAllJobByCompanyAddress(address,pageable);
+
+    public Page<Job> findALlByCompanyAddress(String address, Pageable pageable) {
+        return jobRepository.findAllJobByCompanyAddress(address, pageable);
     }
-    public Page<Job> findAllByCareer(Long id,Pageable pageable){
-        return jobRepository.findAllByCareer_Id(id,pageable);
+
+    public Page<Job> findAllByCareer(Long id, Pageable pageable) {
+        return jobRepository.findAllByCareer_Id(id, pageable);
     }
-    public Page<Job> findAllByCity(Long id,Pageable pageable){
-        return jobRepository.findAllByCompany_City_Id(id,pageable);
+
+    public Page<Job> findAllByCity(Long id, Pageable pageable) {
+        return jobRepository.findAllByCompany_City_Id(id, pageable);
     }
-    public Page<Job> findALlByEmployeeType(Long id,Pageable pageable){
-        return jobRepository.findAllByEmployeeType_Id(id,pageable);
+
+    public Page<Job> findALlByEmployeeType(Long id, Pageable pageable) {
+        return jobRepository.findAllByEmployeeType_Id(id, pageable);
     }
-    public List<Job> findBySalary(Double min,Double max){
+
+    public List<Job> findBySalary(Double min, Double max) {
         List<Job> jobs = findAllTest();
         List<Job> findList = new ArrayList<>();
-        for (Job j:jobs) {
-            if (j.getSalaryMin()<=min || j.getSalaryMax()<=max){
+        for (Job j : jobs) {
+            if (j.getSalaryMin() <= min || j.getSalaryMax() <= max) {
                 findList.add(j);
             }
         }
         return findList;
     }
 
-    public void copy(List<Job> jobList,List<Job> list){
-        for (Job j:list) {
-            if (!jobList.contains(j)){
+    public void copy(List<Job> jobList, List<Job> list) {
+        for (Job j : list) {
+            if (!jobList.contains(j)) {
                 jobList.add(j);
             }
         }
     }
 
 
-    public Double[] checkSalary(Long salary){
+    public Double[] checkSalary(Long salary) {
         Double[] minMax = new Double[2];
-        if (salary==1){
+        if (salary == 1) {
             minMax[0] = 10000D;
             minMax[1] = 20000D;
         }
-        if (salary==2){
+        if (salary == 2) {
             minMax[0] = 20000D;
             minMax[1] = 30000D;
         }
-        if (salary==3){
+        if (salary == 3) {
             minMax[0] = 30000D;
             minMax[1] = 40000D;
         }
-        if (salary==4){
+        if (salary == 4) {
             minMax[0] = 40000D;
             minMax[1] = 50000D;
         }
-        if (salary==5){
+        if (salary == 5) {
             minMax[0] = 50000D;
             minMax[1] = 60000D;
         }
         return minMax;
     }
 
-    public List<Job> searchAllFields(SearchAll searchAll){
+    public List<Job> searchAllFields(SearchAll searchAll) {
         Integer count = 0;
         List<Job> searchList = new ArrayList<>();
         List<String> expiration = searchAll.getExpiration();
-        if (!expiration.isEmpty()){
-            for (String e: expiration) {
+        if (!expiration.isEmpty()) {
+            for (String e : expiration) {
                 List<Job> listFind = jobRepository.findAllByExpiration(Long.parseLong(e));
-                copy(searchList,listFind);
+                copy(searchList, listFind);
             }
-        }else {
-            count ++;
+        } else {
+            count++;
         }
         List<String> cities = searchAll.getCity();
-        if (!cities.isEmpty()){
-            for (String c:cities){
+        if (!cities.isEmpty()) {
+            for (String c : cities) {
                 List<Job> listFind = jobRepository.findAllByCompany_City_Id(Long.parseLong(c));
-                copy(searchList,listFind);
+                copy(searchList, listFind);
             }
-        }else {
+        } else {
             count++;
         }
         List<String> typeTime = searchAll.getTypeTime();
-        if (!typeTime.isEmpty()){
-            for (String t:typeTime){
+        if (!typeTime.isEmpty()) {
+            for (String t : typeTime) {
                 List<Job> listFind = jobRepository.findAllByTypeTime(t);
-                copy(searchList,listFind);
+                copy(searchList, listFind);
             }
-        }else {
+        } else {
             count++;
         }
         List<String> gender = searchAll.getGender();
-        if (!gender.isEmpty()){
-            for (String g: gender){
+        if (!gender.isEmpty()) {
+            for (String g : gender) {
                 List<Job> listFind = jobRepository.findAllByGender(g);
-                copy(searchList,listFind);
+                copy(searchList, listFind);
             }
-        }else {
+        } else {
             count++;
         }
         List<String> salaries = searchAll.getSalary();
-        if (!salaries.isEmpty()){
-            for (String s:salaries){
+        if (!salaries.isEmpty()) {
+            for (String s : salaries) {
                 Double min = checkSalary(Long.parseLong(s))[0];
                 Double max = checkSalary(Long.parseLong(s))[1];
-                List<Job> listFind = findBySalary(min,max);
-                copy(searchList,listFind);
+                List<Job> listFind = findBySalary(min, max);
+                copy(searchList, listFind);
             }
-        }else {
+        } else {
             count++;
         }
-        if (count==5){
-            searchList=findAllTest();
+        if (count == 5) {
+            searchList = findAllTest();
 
         }
         return searchList;
@@ -144,49 +152,56 @@ public class JobService extends ICoreServiceJob{
 
     public List<Job> searchAllByKey(String key1, String key2) {
         List<Job> jobsByKeyWord = new ArrayList<>();
-        if (key2!=null && key1!=null){
+        if (!key2.equals("") && !key1.equals("")) {
             List<Job> jobs = jobRepository.findAllByCompany_City_Id(Long.parseLong(key2));
-            for (Job j: jobs) {
-                if (j.getCompany().getAccount().getName().toUpperCase().contains(key1.toUpperCase())){
+            for (Job j : jobs) {
+                if (j.getCompany().getAccount().getName().toUpperCase().contains(key1.toUpperCase())) {
                     List<Job> findList = new ArrayList<>();
                     findList.add(j);
-                    copy(jobsByKeyWord,findList);
+                    copy(jobsByKeyWord, findList);
                 }
-                if (j.getEmployeeType().getName().toUpperCase().contains(key1.toUpperCase())){
+                if (j.getEmployeeType().getName().toUpperCase().contains(key1.toUpperCase())) {
                     List<Job> findList = new ArrayList<>();
                     findList.add(j);
-                    copy(jobsByKeyWord,findList);
+                    copy(jobsByKeyWord, findList);
                 }
-                if (j.getGender().toUpperCase().contains(key1.toUpperCase())){
+                if (j.getGender().equalsIgnoreCase(key1)) {
                     List<Job> findList = new ArrayList<>();
                     findList.add(j);
-                    copy(jobsByKeyWord,findList);
+                    copy(jobsByKeyWord, findList);
                 }
-
             }
-        }else {
-            if (key1!=null){
+        } else {
+            if (!key1.equals("")) {
                 List<Job> jobs = jobRepository.findAll();
-                for (Job j: jobs) {
-                    if (j.getCompany().getAccount().getName().toUpperCase().contains(key1.toUpperCase())){
+                for (Job j : jobs) {
+                    if (j.getCompany().getAccount().getName().toUpperCase().contains(key1.toUpperCase())) {
                         jobsByKeyWord.add(j);
                     }
-                    if (j.getEmployeeType().getName().toUpperCase().contains(key1.toUpperCase())){
+                    if (j.getEmployeeType().getName().toUpperCase().contains(key1.toUpperCase())) {
                         jobsByKeyWord.add(j);
                     }
-                    if (j.getGender().toUpperCase().contains(key1.toUpperCase())){
+                    if (j.getGender().equalsIgnoreCase(key1)) {
                         jobsByKeyWord.add(j);
                     }
-               }
+                }
+                return jobsByKeyWord;
             }
-            if (key2!=null){
-                 jobsByKeyWord = jobRepository.findAllByCompany_City_Id(Long.parseLong(key2));
+            if (!key2.equals("")) {
+                jobsByKeyWord = jobRepository.findAllByCompany_City_Id(Long.parseLong(key2));
+                return jobsByKeyWord;
             }
-            if (key1==null && key2==null){
-                jobsByKeyWord = jobRepository.findAll();
-
-            }
+            jobsByKeyWord = jobRepository.findAll();
         }
         return jobsByKeyWord;
+        //Ẩn bài đăng có status false
+//        int index = -1;
+//        for (int i = 0; i < jobsByKeyWord.size(); i++) {
+//            if (jobsByKeyWord.get(i).getStatus()) {
+//                index = i;
+//            }
+//            jobsByKeyWord.remove(index);
+//        }
+//        return jobsByKeyWord;
     }
 }
