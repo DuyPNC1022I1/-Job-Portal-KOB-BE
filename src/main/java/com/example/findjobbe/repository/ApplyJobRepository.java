410bb76624aa74eb1d71d25b4f3e5325f54f33c0
dung021194 <dung3864.99@gmail.com>
2023-04-19 22:54:25 +0700

    add apply

--- src/main/java/com/example/findjobbe/repository/ApplyJobRepository.java
+++ src/main/java/com/example/findjobbe/repository/ApplyJobRepository.java
@@ -1,9 +1,12 @@
 package com.example.findjobbe.repository;
 
 import com.example.findjobbe.model.ApplyJob;
+import com.example.findjobbe.model.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ApplyJobRepository extends JpaRepository<ApplyJob,Long> {
+    public boolean existsByJob_IdAndUser_Id(Long jobId,Long userId);
+    public ApplyJob findApplyJobByJob_IdAndUser_Id(Long jobId,Long userId);
 }
--- src/main/java/com/example/findjobbe/service/ApplyJob/ApplyJobService.java
+++ src/main/java/com/example/findjobbe/service/ApplyJob/ApplyJobService.java
@@ -1,31 +1,85 @@
 package com.example.findjobbe.service.ApplyJob;
 
-import com.example.findjobbe.model.Job;
-import com.example.findjobbe.model.User;
+import com.example.findjobbe.model.*;
 import com.example.findjobbe.repository.ApplyJobRepository;
-import com.example.findjobbe.repository.JobRepository;
-import com.example.findjobbe.repository.UserRepository;
+import com.example.findjobbe.service.CompanyService;
+import com.example.findjobbe.service.ICoreService;
 import com.example.findjobbe.service.UserService;
 import com.example.findjobbe.service.jobs.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ApplyJobService {
+public class ApplyJobService implements ICoreService<ApplyJob> {
     @Autowired
     ApplyJobRepository applyJobRepository;
     @Autowired
     UserService userService;
     @Autowired
     JobService jobService;
-
-    public boolean applyJob(Long userId,Long jobId){
-        User user = userService.findOne(userId);
-        Job job = jobService.findOne(jobId);
-        if (user!=null && job!=null){
+    @Autowired
+    CompanyService companyService;
 
 
-        }
+    @Override
+    public Page<ApplyJob> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public ApplyJob findOne(Long id) {
+        return applyJobRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void save(ApplyJob applyJob) {
+        applyJobRepository.save(applyJob);
+    }
+
+    @Override
+    public void delete(Long id) {
+        applyJobRepository.deleteById(id);
 
     }
+    public boolean applyJob(Long jobId,Long userId){
+        User user = userService.findOne(userId);
+        Job job = jobService.findOne(jobId);
+        ApplyJob applyJobFind = applyJobRepository.findApplyJobByJob_IdAndUser_Id(jobId,userId);
+        if (user!=null && job!=null && applyJobFind==null){
+            ApplyJob applyJob = new ApplyJob();
+            applyJob.setUser(user);
+            applyJob.setJob(job);
+            applyJob.setStatus("Pending");
+            applyJobRepository.save(applyJob);
+            Notification notification = new Notification();
+            String text = user.getAccount().getName() + "applied for"+ job.getCareer().getName()
+                           +job.getCompany().getAccount().getName() +", please check.";
+            notification.setText(text);
+            notification.setStatus(true);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean CancelApplyJob(Long userId,Long jobId){
+        User user = userService.findOne(userId);
+        Job job = jobService.findOne(jobId);
+        ApplyJob applyJobFind = applyJobRepository.findApplyJobByJob_IdAndUser_Id(jobId,userId);
+        if (user!=null && job!=null && applyJobFind != null){
+                applyJobFind.setStatus("Canceled");
+                applyJobRepository.save(applyJobFind);
+                Notification notification = new Notification();
+                String text = user.getAccount().getName() + " canceled "+ job.getCareer().getName()
+                        + job.getCompany().getAccount().getName() + " !";
+                notification.setText(text);
+                notification.setStatus(true);
+                return true;
+        }
+        return false;
+    }
+
+
+
 }
