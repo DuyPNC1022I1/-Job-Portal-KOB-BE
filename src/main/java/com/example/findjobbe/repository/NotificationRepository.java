23fdec807e52442364a0e374c35a94895d87b62f
dung021194 <dung3864.99@gmail.com>
2023-04-25 21:48:04 +0700

    fix noti

--- src/main/java/com/example/findjobbe/repository/NotificationRepository.java
+++ src/main/java/com/example/findjobbe/repository/NotificationRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification,Long> {
-    public List<Notification> findAllByCompany_Id(Long id);
-    public List<Notification> findAllByUser_Id(Long id);
+    public List<Notification> findAllByCompany_IdOrderByIdDesc(Long id);
+    public List<Notification> findAllByUser_IdOrderByIdDesc(Long id);
 
 }
